2f525a1c655a0dea10668439c0bfd271eb117990
huangdianchun <simpleotter23@gmail.com>
2013-01-25 17:51:48 +0800

    add legenditem

---
+++ components/curveanalyzer/legenditem/legenditem.h
@@ -0,0 +1,40 @@
+#ifndef _LEGENDITEM_H_
+#define _LEGENDITEM_H_
+
+#include <QtGui/QWidget>
+
+class QLabel;
+class QResizeEvent;
+class ColorBlock;
+
+class LegendItem : public QWidget
+{
+public:
+    LegendItem(const QColor &color, const QString &label, QWidget *parent = 0);
+
+protected:
+	void resizeEvent(QResizeEvent *event);
+	
+private:
+	ColorBlock *colorBlock_;
+	QLabel *label_;
+};
+
+#endif /* _LEGENDITEM_H_ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
